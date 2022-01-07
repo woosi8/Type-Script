fe1eca8eb9de7398fbb15abc89c8b88e549c46ca
DESKTOP-QNTD4OJ\Asher <tmfvmehek@gmail.com>
2022-01-07 22:46:16 +0900

    type-script-lecture

---
+++ main.js
@@ -0,0 +1,7 @@
+console.log("HelloWorld1~~~111222");
+var Car = /** @class */ (function () {
+    function Car() {
+        this.engine = 1;
+    }
+    return Car;
+}());
---
+++ main.ts
@@ -0,0 +1,8 @@
+console.log("HelloWorld1~~~111222");
+
+class Car {
+	engine: number;
+	constructor() {
+		this.engine = 1;
+	}
+}
