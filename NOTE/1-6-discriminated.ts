f679d1ec6fd0362409f6bad320c4724c9809f52c
DESKTOP-QNTD4OJ\Asher <tmfvmehek@gmail.com>
2022-01-08 13:02:58 +0900

    1-8

--- NOTE/1-6-discriminated.ts
+++ NOTE/1-6-discriminated.ts
@@ -1,34 +1,36 @@
 {
-  // function: login -> success, fail ⏱
-  type SuccessState = {
-    result: 'success';
-    response: {
-      body: string;
-    };
-  };
-  type FailState = {
-    result: 'fail';
-    reason: string;
-  };
-  type LoginState = SuccessState | FailState;
+	// function: login -> success, fail ⏱
+	type SuccessState = {
+		result: "success";
+		response: {
+			body: string;
+		};
+	};
+	type FailState = {
+		result: "fail";
+		reason: string;
+	};
+	type LoginState = SuccessState | FailState;
 
-  function login(): LoginState {
-    return {
-      result: 'success',
-      response: {
-        body: 'logged in!',
-      },
-    };
-  }
+	function login(): LoginState {
+		return {
+			result: "success",
+			response: {
+				body: "logged in!",
+			},
+			// result: 'fail',
+			// reason: "failed"
+		};
+	}
 
-  // printLoginState(state: LoginState)
-  // success -> 🎉 body
-  // fail -> 😭 reason
-  function printLoginState(state: LoginState) {
-    if (state.result === 'success') {
-      console.log(`🎉 ${state.response.body}`);
-    } else {
-      console.log(`😭 ${state.reason}`);
-    }
-  }
+	// printLoginState(state: LoginState)
+	// success -> 🎉 body
+	// fail -> 😭 reason
+	function printLoginState(state: LoginState) {
+		if (state.result === "success") {
+			console.log(`🎉 ${state.response.body}`);
+		} else {
+			console.log(`😭 ${state.reason}`);
+		}
+	}
 }
--- NOTE/1-8-enum.ts
+++ NOTE/1-8-enum.ts
@@ -1,33 +1,34 @@
 {
-  /**
-   * Enum
-   */
-  // JavaScript
-  const MAX_NUM = 6;
-  const MAX_STUDENTS_PER_CLASS = 10;
-  const MONDAY = 0;
-  const TUESDAY = 1;
-  const WEDNESDAY = 2;
-  const DAYS_ENUM = Object.freeze({ MONDAY: 0, TUESDAY: 1, WEDNESDAY: 2 });
-  const dayOfToday = DAYS_ENUM.MONDAY;
+	/**
+	 * Enum
+	 */
+	// JavaScript
+	const MAX_NUM = 6;
+	const MAX_STUDENTS_PER_CLASS = 10;
+	const MONDAY = 0;
+	const TUESDAY = 1;
+	const WEDNESDAY = 2;
+	const DAYS_ENUM = Object.freeze({ MONDAY: 0, TUESDAY: 1, WEDNESDAY: 2 });
+	const dayOfToday = DAYS_ENUM.MONDAY;
 
-  // TypeScript
-  type DaysOfWeek = 'Monday' | 'Tuesday' | 'Wednesday';
-  enum Days {
-    Monday,
-    Tuesday,
-    Wednesday,
-    Thursday,
-    Friday,
-    Saturday,
-    Sunday,
-  }
-  console.log(Days.Monday);
-  let day: Days = Days.Saturday;
-  day = Days.Tuesday;
-  day = 10;
-  console.log(day);
+	// TypeScript
+	type DaysOfWeek = "Monday" | "Tuesday" | "Wednesday"; //Enum보다 사용되는(타입을 보장해줌)
+	enum Days {
+		// Monday =1, 1부터 시작을 원할때, 문자열도 가능 "monday", 근데 다 넣어줘야 하는 번거로움이 있음
+		Monday,
+		Tuesday,
+		Wednesday,
+		Thursday,
+		Friday,
+		Saturday,
+		Sunday,
+	}
+	console.log(Days.Monday);
+	let day: Days = Days.Saturday;
+	day = Days.Tuesday;
+	day = 10; //enum의 문제점 : 숫자를 맘대로 넣을수가 있다
+	console.log(day);
 
-  let dayOfweek: DaysOfWeek = 'Monday';
-  dayOfweek = 'Wednesday';
+	let dayOfweek: DaysOfWeek = "Monday";
+	dayOfweek = "Wednesday";
 }
