c82468db2d618348885cecda66b3a36515ad07d6
DESKTOP-QNTD4OJ\Asher <tmfvmehek@gmail.com>
2022-01-18 13:19:23 +0900

    start appl coading

---
+++ Apple/index.js
@@ -0,0 +1,17 @@
+// termianl : tsc -w
+var 배열 = ["kim", "park"];
+var 오브 = { name: "kim" };
+var 이름 = "kim";
+// let 이름: string | number = "kim";
+function 함수(x) {
+    return x * 2;
+}
+함수(123);
+var john = [3, false];
+var johndo = { name: "kim" };
+var User = /** @class */ (function () {
+    function User(name) {
+        this.name = name;
+    }
+    return User;
+}());
---
+++ Apple/index.ts
@@ -0,0 +1,28 @@
+// termianl : tsc -w
+let 배열: string[] = ["kim", "park"];
+let 오브: { name: string } = { name: "kim" };
+// let 오브: { name?: string } = { name: "kim" }; ?옵션
+type Name = string | number;
+let 이름: Name = "kim";
+// let 이름: string | number = "kim";
+
+function 함수(x: number): number {
+	return x * 2;
+}
+
+함수(123);
+
+type Member = [number, boolean];
+
+let john: Member = [3, false];
+
+type MemberOb = { [key: string]: string }; //글자로된 모든 object 속성의 타입은 string이다
+
+let johndo: MemberOb = { name: "kim" };
+
+class User {
+	name: string;
+	constructor(name: string) {
+		this.name = name;
+	}
+}
