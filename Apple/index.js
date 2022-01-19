7ed07e42bf85ddfb1b8ee70d0d4e46cf27079d95
DESKTOP-QNTD4OJ\Asher <tmfvmehek@gmail.com>
2022-01-19 15:00:03 +0900

    typescript untill Narrowing

--- Apple/index.js
+++ Apple/index.js
@@ -1,17 +1,23 @@
-// termianl : tsc -w
-var 배열 = ["kim", "park"];
-var 오브 = { name: "kim" };
+// tsc -w
 var 이름 = "kim";
-// let 이름: string | number = "kim";
-function 함수(x) {
-    return x * 2;
-}
-함수(123);
-var john = [3, false];
-var johndo = { name: "kim" };
-var User = /** @class */ (function () {
-    function User(name) {
-        this.name = name;
-    }
-    return User;
-}());
+var 나이 = 50;
+var 결혼여부 = true;
+var 회원들 = ["kim", "park"];
+// let 회원들2: { merber1: string; merber2: string } = {
+var 회원들2 = {
+    merber1: "kim",
+    merber2: "park",
+};
+var proejct = {
+    member: ["kim", "park"],
+    days: 30,
+    started: true,
+};
+// Union, any, unknown;
+// let 회원들3: { merber1: string; merber2: string } = {};
+var 회원들4 = [1, 2, "hi"];
+var object = { a: "bye" };
+var 이름2; //실드 해제, 다 들어오게 해준다
+var 이름3; //any랑 같다만 조금더 안전하다.
+var 나이2;
+// 나이 + 1 안된다. union타입은 안된다.
--- Apple/index.ts
+++ Apple/index.ts
@@ -1,28 +1,75 @@
-// termianl : tsc -w
-let 배열: string[] = ["kim", "park"];
-let 오브: { name: string } = { name: "kim" };
-// let 오브: { name?: string } = { name: "kim" }; ?옵션
-type Name = string | number;
-let 이름: Name = "kim";
-// let 이름: string | number = "kim";
+// tsc -w
+let 이름: string = "kim";
+let 나이: number = 50;
+let 결혼여부: boolean = true;
+let 회원들: string[] = ["kim", "park"];
+// let 회원들2: { merber1: string; merber2: string } = {
+let 회원들2: { [key: string]: string } = {
+	merber1: "kim",
+	merber2: "park",
+};
 
-function 함수(x: number): number {
+let proejct: { member: string[]; days: number; started: boolean } = {
+	member: ["kim", "park"],
+	days: 30,
+	started: true,
+};
+
+//// // // // //  Union, any, unknown;
+
+// let 회원들3: { merber1: string; merber2: string } = {};
+let 회원들4: (number | string)[] = [1, 2, "hi"];
+let object: { a: string | number } = { a: "bye" };
+let 이름2: any; //실드 해제, 다 들어오게 해준다
+let 이름3: unknown; //any랑 같다만 조금더 안전하다.
+let 나이2: string | number;
+// 나이 + 1 안된다. union타입은 안된다.
+
+// // // // // // //  Function
+
+function 함수2(x: number): number {
 	return x * 2;
 }
+//리턴이 없는 경우 (리턴하면 에러가 나오게)
+function 함수3(x: number): void {
+	x * 2;
+}
+// 파라미터 없어도 되게 할떄는 ?
+function 함수4(x?: number): void {
+	x * 2;
+}
+//  위에랑 같다
+//  function 함수4(x: number | undefined): void {
+// 	x * 2;
+// }
 
-함수(123);
-
-type Member = [number, boolean];
-
-let john: Member = [3, false];
-
-type MemberOb = { [key: string]: string }; //글자로된 모든 object 속성의 타입은 string이다
-
-let johndo: MemberOb = { name: "kim" };
-
-class User {
-	name: string;
-	constructor(name: string) {
-		this.name = name;
+// Narrowing :Type이 아직 하나로 확정되지 않았을 경우
+function 함수5(x: number | string) {
+	if (typeof x === "string") {
+		console.log(x + 3);
+	} else {
+		return x + 1;
 	}
 }
+
+function 함수6(x: number | string) {
+	let array: number[] = [];
+	if (typeof x === "number") {
+		array[0] = x;
+	}
+}
+// assertion * 무슨 타입이 들어올지 확실할때만 쓴다
+function 함수7(x: number | string) {
+	let array: number[] = [];
+	array[0] = x as number; //number type이 됨
+}
+
+console.log(함수2(2));
+
+// type alias
+
+type Animal = string | number | undefined;
+type Animal2 = { name: string; age: number };
+
+let 동물: Animal = "kim";
+let 동물2: Animal2 = { name: "kim", age: 30 };
