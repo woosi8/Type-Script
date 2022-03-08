1ee2afa5ca2d14831fc522493d1b8444147c87b0
DESKTOP-QNTD4OJ\Asher <tmfvmehek@gmail.com>
2022-03-08 16:42:21 +0900

    10-2 module : 파일들간의 중복 이름 충돌 방지, 코드 분리, 모듈간 재사용성

--- NOTE/10-javascript/10-2-this.js
+++ NOTE/10-javascript/10-2-this.js
@@ -1,18 +1,19 @@
 console.log(this);
 
 function simpleFunc() {
-  console.log(this);
+	console.log(this);
 }
 window.simpleFunc();
 console.clear();
 class Counter {
-  count = 0;
-  increase = () => {
-    // console.log(this);
-  };
+	count = 0;
+	increase = () => {
+		// console.log(this);
+	};
 }
 const counter = new Counter();
 counter.increase();
+// const ,let으로 선언한 변수는 window에 없이 때문에 undefined가 된다
 const caller = counter.increase;
 //const caller = counter.increase.bind(counter);
 caller();
